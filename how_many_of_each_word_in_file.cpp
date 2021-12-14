ad6ff79b293bbe55598b147163ea9778a2933894
jindastrialman <keklolich@gmail.com>
2021-12-14 18:38:13 +0300

    Задание выполнено + хороший test.txt

--- how_many_of_each_word_in_file.cpp
+++ how_many_of_each_word_in_file.cpp
@@ -1,46 +1,70 @@
 #include <fstream> // Нужен для ввода из файла
 #include <iostream> // нужен для вывода в консоль
-#include <string> // Полезные функции обработки строки
+#include <vector> // объект похожий на массив, меняющий размер
+#include <map> // словарь
 
 using namespace std;
 
 string const filename = "test.txt";
 
-string to_lowercase(string str) // как оказалось в изначальном c++ нет to_lower для строк
+string to_lowercase(string &str) // как оказалось в изначальном c++ нет to_lower для строк
 {
-	for(auto iter = str.begin(); iter != str.end(); iter++)
-		*iter = tolower[*iter];
+	for(int i = 0; i < str.size(); i++)
+		str[i] = tolower(str[i]);
 	return str;
 }
 
-vector<string> split(string str) // то же можно сказать про split (здесь только изначально извесный набор символов(маленькие буквы))
+vector<string> split(string &str) // то же можно сказать про split (здесь только изначально извесный набор символов(маленькие буквы))
 {
-	auto result = new vector<string>();
+	vector<string> result = vector<string>();
 
-	for(auto iter = str.begin(); iter != str.end(); iter++)
+	for(int i = 0; i < str.size(); i++)
 	{
-		if(*iter >= 'a' && *iter <= 'z')
-		{	
-			auto start_of_word = iter;
-		}
+		string word = "";
+		while(str[i] >= 'a' && str[i] <= 'z' && i < str.size())
+			word += str[i++]; // если подходящие буквы и не конец строки то добавляю их в слово
 
+		if(word != "")
+			result.push_back(word); // запись в конец
 	}
+
+	return result;
 }
 
 int main()
 {
-	auto file_input_stream = new fstream(filename, input.in); //
-	auto word_dictionary = new map<string, int>();  // словарб
-	auto words_met = new vector<string>(); // для ключей к словарю
+	fstream file_input_stream;
+	file_input_stream.open(filename, fstream::in); //
 
-	if(!file_input.is_open())
+	map<string, int> word_dictionary = map<string, int>();  // словарь
+	vector<string> words_met = vector<string>(); // для ключей к словарю
+
+	if(!file_input_stream.is_open()) // если файл не открылся, то и читать нечего
 		return -1;
-
-	while(file_input_stream.eof())
+	
+	while(!file_input_stream.eof())
 	{
 		string temporary_input;
 		file_input_stream >> temporary_input;
+		temporary_input = to_lowercase(temporary_input); // все заглавные становятся прописными
+		auto splited_input = split(temporary_input); // вектор со словами (или одним словом если оно было без разделителей вродед)
 
+		for(auto i = splited_input.begin(); i != splited_input.end(); i++) // с помощью итератеров пробег по вектору из разделённых слов
+			if(word_dictionary[*i]++ == 0) // если в словаре по ключу лежал 0, то он запишется в вектор (в любом случае по ключу значение увеличится на 1)
+				words_met.push_back(*i);
 
 	}
+
+	file_input_stream.close(); // лишним не будет закрыть не нужный файл
+
+	cout << "(";
+	for(auto i = words_met.begin(); i != words_met.end(); i++)
+	{
+		cout << "[\"" <<*i<<"\", "<< word_dictionary[*i] << "]";
+		if(i+1 != words_met.end())
+			cout << ", "; //вывод в правильном формате
+	}
+	cout << ")\n";
+
+	
 }
